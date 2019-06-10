9e6a4ba5419bb202c264828d5db385622146224c
Elijah Kotyluk <elijah@elijahkotyluk.com>
2019-06-10 00:33:28 -0700

    Basic initial configuration:  - Add webpack, babel, and eslint  - Create basic project structure  - Create nameless constructor

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const Nameless = (function () {
+  function createComponent(type, attrs = {}, ...children) {
+
+  }
+
+  return createComponent
+}());
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'index.bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "eslint-loader"
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+    ]
+  }
+}
