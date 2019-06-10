54dad79892eb133140146c8735e80c04590660b7
Elijah Kotyluk <elijah@elijahkotyluk.com>
2019-06-10 08:06:25 -0700

    Add very basic createComponent function.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
-const Nameless = (function () {
-  function createComponent(type, attrs = {}, ...children) {
+/* Nameless constructor: */
 
+const Nameless = (function () {
+
+  /* Create component */
+  function createComponent(type, attributes = {}, ...children) {
+    const childElements = [].concat(...children).map(
+      child => {
+        if (child != null && child != true && child != false) {
+          return child instanceof Object
+            ? child
+            : createComponent("text", {
+              textContent: child
+            })
+        }
+      });
+    return {
+      type,
+      children: childElements,
+      props: object.assign({ children: childElements }, attributes)
+    }
   }
 
   return createComponent
