eb9641f6aa0c124829cebb760ecff9348f34836a
Elijah Kotyluk <elijah@elijahkotyluk.com>
2019-06-26 00:47:40 -0700

    Update playground example:  - Include render  method in example to test visually

--- playground/createElement/src/index.js
+++ playground/createElement/src/index.js
@@ -1,11 +1,15 @@
 import createElement from '../../../src/createElement'
+import render from '../../../src/render'
 
+console.log('render', render)
 const root = document.getElementById('root')
 
-var test = (
-  <div>
-    <p>Hello World!</p>
-  </div>
-)
+class Component{}
 
-console.log('test element:', test)
+class HelloWorld extends Component {
+    render() {
+        return <div style={{ color: 'blue' }}>Hello World</div>
+    }
+}
+
+render(<HelloWorld/>, document.getElementById("root"))
