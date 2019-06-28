3aca1907970104a81a21b61ae72613983383250b
Elijah Kotyluk <elijah@elijahkotyluk.com>
2019-06-27 17:39:26 -0700

    Update comments.

--- src/createElement.js
+++ src/createElement.js
@@ -11,8 +11,14 @@ export default function createElement(type, props, ...children) {
     // {array}: Contains children of created element.
     let childElements = []
 
+    // iterate through children;
     for(let i = 0; i < children.length; i++){
+        // If current index of children is a Boolean, Undefined, or Null continue to next iteration:
         if (typeof children[i] === 'boolean' || children[i] === undefined || children === null) continue
+
+        // If current index of children is an Array =>
+        // concatenate the current index with childElements;
+        // else => push the current index into childElements;
         if(children[i] instanceof Array) {
             childElements = childElements.concat(children[i])
         } else {
