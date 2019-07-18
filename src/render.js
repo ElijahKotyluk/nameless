bc736c2d985846d0ad5e001e53e356b1395df980
Elijah Kotyluk <elijah@elijahkotyluk.com>
2019-07-18 00:02:51 -0700

    feat(Render): Update render.

--- src/render.js
+++ src/render.js
@@ -1,58 +1,87 @@
-export default function render(vnode, parent) {
+/*
+ * Render method:
+ *
+ * @param { string || fn } vnode: Node to be rendered.
+ * @param { object } parent: Parent node of node being rendered.
+*/
 
-  let dom
+export default function render(vnode, parent, component, oldDom) {
 
-  if (typeof vnode == 'string') {
-    dom = document.createTextNode(vnode)
-    parent.appendChild(dom)
+  // dom var;
+  let dom;
+
+  // if our passed vnode's value is a 'String' value =>
+  // we create a text node using the passed vnode param and
+  // and assign our newly created text node to our dom variable
+  // then we append the assigned dom variable as a child of the passed parent param;
+  //
+  // else if the vnode's nodeName property's value is a string =>
+  //
+  if (typeof vnode == 'string' || typeof vnode == 'number') {
+    dom = document.createTextNode(vnode);
+    component && (component.__rendered = dom);
+    parent.appendChild(dom);
+
+    if (oldDom) {
+      parent.replaceChild(dom, oldDom);
+    } else {
+      parent.appendChild(dom);
+    }
 
   } else if (typeof vnode.nodeName == 'string') {
-    dom = document.createElement(vnode.nodeName)
-    setAttrs(dom, vnode.props)
-    parent.appendChild(dom)
+    dom = document.createElement(vnode.nodeName);
+    component && (component.__rendered = dom);
+
+    setAttrs(dom, vnode.props);
+    parent.appendChild(dom);
 
     for (let i = 0; i < vnode.children.length; i++) {
-      render(vnode.children[i], dom)
+      render(vnode.children[i], dom, null, null);
     }
 
   } else if (typeof vnode.nodeName == 'function') {
-    let fn = vnode.nodeName
-    let innerVnode = fn.prototype.render()
-    render(innerVnode, parent)
+    let fn = vnode.nodeName;
+    
+    let instance = new func(vnode.props);
+    comp && (comp.__rendered = instance);
+
+    let innerVnode = inst.render(instance);
+    render(innerVnode, parent, instance, oldDom);
   }
 }
 
 
 function setAttrs(dom, props) {
-    const propKeys = Object.keys(props)
+    const propKeys = Object.keys(props);
+    
     propKeys.forEach(key => {
-        const value = props[key]
+        const value = props[key];
 
         if(key == "className") {
-            dom.setAttribute("class", value)
-            return
+            dom.setAttribute("class", value);
+            return;
         }
 
         if(key == "style") {
             if(typeof value == "string") {
-                dom.style.cssText = value
+                dom.style.cssText = value;
             }
 
             if(typeof value == "object") {
                 for (let i in value) {
-                    dom.style[i] =  value[i]
+                    dom.style[i] =  value[i];
                 }
             }
-            return
+            return;
 
         }
 
         if(key[0] == "o" && key[1] == "n") {
-            const capture = (key.indexOf("Capture") != -1)
-            dom.addEventListener(key.substring(2).toLowerCase(), capture)
-            return
+            const capture = (key.indexOf("Capture") != -1);
+            dom.addEventListener(key.substring(2).toLowerCase(), value, capture);
+            return;
         }
 
-        dom.setAttribute(key, value)
+        dom.setAttribute(key, value);
     })
 }
