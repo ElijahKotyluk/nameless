ce208fb273704a12436a9c4b325d68adc807cd08
Elijah Kotyluk <elijah@elijahkotyluk.com>
2019-07-18 07:40:57 -0700

    test(createElement): Remove clutter

--- test/createElement.spec.js
+++ test/createElement.spec.js
@@ -5,14 +5,8 @@ describe('createElement(jsx)', () => {
   let el;
 
   it('should return an element', () => {
-
-    // createElement function does not throw error:
     expect( () => el = createElement('test') ).not.toThrow();
-
-    // el is an object:
     expect(el).toEqual(jasmine.any(Object));
-
-    // el should contain the following properties:
     expect(Object.keys(el)).toContain('children');
     expect(Object.keys(el)).toContain('props');
     expect(Object.keys(el)).toContain('nodeName');
